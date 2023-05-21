7a447626cd3844c14055695af52a0a9cdeb83d4f
Tim Gold <timothygold@comcast.net>
2023-05-21 14:25:58 -0400

    updated paragraph on aboutme.js to reflect my journey

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -3,14 +3,16 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me </h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        For the past eleven years I have been working in the non-profit sector. The highlight for me has been the
+        ability to meet people from all across the globe. The decision was made to shift my career to a passion
+        of mine coding. Specifically to learn web development and receive training in HTML, CSS, and JavaScript.
+        This is why I chose to enroll in the full-stack bootcamp to gain valuable training and experience in the
+        deployment of webpages. The experience I have gained over the years is demonstrated in my leadership
+        ability, teaching others, being able to collaborate well wtih a team, a passion to fulfill the mission, and the trust to complete the
+        task. I am a family man with a wife, and two sons living in the Philadelphia area. I have the drive to succeed
+        and I look forward to being a part of the world of web development.
       </p>
     </div>
   );
