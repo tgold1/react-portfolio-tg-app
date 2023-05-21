e721a48a4574937926445867b9d0f1706ec9872e
Tim Gold <timothygold@comcast.net>
2023-05-21 19:00:51 -0400

    added code to contact.js to establish input fields for name, email, message

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+import { validateEmail } from '../../utils/helpers';
+
+export default function Form() {
+  const [Name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'Name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue)
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    
+    }
+    alert (`Hello ${Name}`);
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form className='form>'>
+        <input
+          value={Name}
+          name='Name'
+          onChange={handleInputChange}
+          type='text'
+          placholder='Name'
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name='message'
+          onChange={handleInputChange}
+          type='text'
+          placeholder='Message'
+        />
+        <button type='button' onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className='error-text'>{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
+
