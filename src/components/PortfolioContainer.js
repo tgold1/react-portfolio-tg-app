b424a9f1f75ba6c70ba885b12b13c6950250c819
Tim Gold <timothygold@comcast.net>
2023-05-21 14:20:12 -0400

    code to portfoliocontainer.js to working links on the four navigation pages and the about me as the page on load

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
+import Resume from './pages/Resume';
+import Aboutme from './pages/Aboutme';
+import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('Aboutme');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Aboutme') {
+      return <Aboutme />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
     return <Contact />;
   };
