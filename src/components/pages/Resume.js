2cfe4c906e32d33c60a5c9cda11dcc5badb3bdf1
Tim Gold <timothygold@comcast.net>
2023-05-21 16:08:42 -0400

    added proficiencies to the resume.js page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,18 +3,27 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Resume</h1>
+
+      <h2>Front-End Proficiencies</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        . HTML<br/>
+        . CSS<br/>
+        . JavaScript<br/>
+        . jquery<br/>
+        . React<br/>
+        . Bootstrap<br/>
+      </p>
+
+      <h2>Back-end Proficiencies</h2>
+      <p>
+        . APIs<br/>
+        . Node<br/>
+        . Express<br/>
+        . MYSQL, Sequelize<br/>
+        . MongoDB, Mongoose<br/>
+        . REST<br/>
+        . GraphQL<br/>
       </p>
     </div>
   );
