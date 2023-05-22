6a191cc1112ff14cdb1674095803c0ac97d20074
Tim Gold <timothygold@comcast.net>
2023-05-22 18:52:00 -0400

    added footer ul to navtabs.js

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,6 +2,14 @@
 import React from 'react';
 import './styles/NavTabs.css';
 
+const styles = {
+  navTabsStyle: {
+    backgroundColor: 'green',
+    justifyContent: 'flex-left',
+    color: 'green',
+    textAlign: 'right',
+  },
+};
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
@@ -49,7 +57,25 @@ function NavTabs({ currentPage, handlePageChange }) {
         </a>
       </li>
     </ul>
+   
   );
+  <footer class="footer">
+
+  <nav>
+      <ul>
+          <li>
+              <a href="https://stackoverflow.com/users/21932519/tgfullstack">Stackoverflow</a>
+          </li>
+          <li>
+              <a href="https:github.com/tgold1">Github</a>
+          </li>
+          <li>
+              <a href="https:linkedin.com/in/tim-gold-81a9164a">Linkedin</a>
+          </li>
+      </ul>
+  </nav>
+  
+  </footer>
 }
 
 export default NavTabs;
