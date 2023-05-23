860d6e59b95655b3b61a351f4c50a264d315cf69
Tim Gold <timothygold@comcast.net>
2023-05-23 14:53:48 -0400

    added header.js to have a header with my name on the top of each page and added function to portfolio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Footer from './footer';
+import Header from './header';
 import Resume from './pages/Resume';
 import Aboutme from './pages/Aboutme';
 import Portfolio from './pages/Portfolio';
@@ -11,7 +12,7 @@ export default function PortfolioContainer() {
   
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    <h1>Tim Gold</h1>
+    
     if (currentPage === 'Resume') {
       return <Resume />;
     }
@@ -27,15 +28,21 @@ export default function PortfolioContainer() {
   const renderFooter = () => {
     return Footer
   }
+
+  const renderHeader = () => {
+    return Header
+  }
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
       {/* We are passing the currentPage from state and the function to update it */}
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
-      {renderPage()}{renderFooter()}
+      {renderPage()}{renderHeader()}
     </div>
   );
+  {renderFooter()}
 }
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function Header() {
+    return (
+        <header class="header">
+
+       <h1> Tim Gold </h1>
+        
+        </header>
+     
+    );
+   
+  }
+  
+
+
+export default Header;
