b91f33677ba4fc5ec48475964e5eb857edb9db5d
Tim Gold <timothygold@comcast.net>
2023-05-23 20:44:20 -0400

    making adjustments to image size in portfolio.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,62 +5,56 @@ import picture3 from '../images/Ghosttrackerscreenshot.png';
 import picture4 from '../images/Notetakerscreenshot.png';
 import picture5 from '../images/weatherdashscreenshot.png';
 import picture6 from '../images/Workdayscreenshot.png';
+import styles from '../styles/styles.css';
+
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <div class="row">
-        <div class="col-sm-6">
-      <div class="card" style={{width: 40}}> 
+      <div class="row col-sm-6" style={{display: 'inline-block'}}>
+        
+      <div class="card"> 
       <a href="https://lit-cliffs-63394.herokuapp.com/" target="_blank">
-      <img src= {picture} alt="picture of text editor" width={200} />JATE Text Editor</a>
-          </div>
+      <img src= {picture} alt="picture of text editor"width={150} />JATE Text Editor</a>
+          
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-6">
-      <div class="card" style={{width: 40}}>
+      <div class="row col-sm-6  "style={{display: 'inline-block'}}>
+      
+      <div class="card">
       <a href="https://stormy-hollows-61177.herokuapp.com/" target="_blank">
-      <img src= {picture2}alt="picture of tech Blog" width={200} />The Tech Blog</a>
-          </div>
+      <img src= {picture2}alt="picture of tech Blog" width={150} />The Tech Blog</a>
+        
         </div>
       </div>
 
       
-      <div class="row">
-        <div class="col-sm-6">
-      <div class="card" style={{width: 40}}>
+      <div class="row col-sm-6"style={{display: 'inline-block'}}> 
+      <div class="card">
       <a href="https://quiet-wave-19391.herokuapp.com/" target="_blank">
-      <img src= {picture3} alt="picture of Ghost Tracker" width={200} />The Ghost Tracker</a>
-          </div>
+      <img src= {picture3} alt="picture of Ghost Tracker" width={150} />The Ghost Tracker</a> 
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-6">
-      <div class="card" style={{width: 40}}>
+      <div class="row col-sm-6 "style={{display: 'inline-block'}}>
+      <div class="card">
       <a href="https://protected-dusk-26151.herokuapp.com/" target="_blank">
-      <img src= {picture4} alt="picture of Note Taker" width={200} />The Note Taker</a>
-          </div>
+      <img src= {picture4} alt="picture of Note Taker" width={150} />The Note Taker</a>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-6">
-      <div class="card" style={{width: 40}}>
+      <div class="row col-sm-6 "style={{display: 'inline-block'}}> 
+      <div class="card">
       <a href=" https://tgold1.github.io/weather-dashboard-api/" target="_blank">
-      <img src= {picture5} alt="picture of Weather Dashboard" width={200} />Weather Dashboard</a>
-          </div>
+      <img src= {picture5} alt="picture of Weather Dashboard" width={150} />Weather Dashboard</a>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-6">
+      <div class="row col-sm-6 "style={{display: 'inline-block'}}>
       <div class="card" style={{width: 40}}>
       <a href="  https://tgold1.github.io/work-day-scheduler/" target="_blank">
-      <img src= {picture6} alt="picture of Work Day Scheduler" width={200} />Workday Scheduler</a>
-          </div>
+      <img src= {picture6} alt="picture of Work Day Scheduler" width={150} />Workday Scheduler</a>
         </div>
       </div>
 
